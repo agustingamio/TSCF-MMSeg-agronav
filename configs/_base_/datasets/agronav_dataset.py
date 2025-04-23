80468e2b8fec32aec95885fc775fd39aef08cd61
Agustin Gamio <agamio@urudatasoftware.com>
2025-04-23 18:11:58 -0300

    fix

--- configs/_base_/datasets/agronav_dataset.py
+++ configs/_base_/datasets/agronav_dataset.py
@@ -16,6 +16,13 @@ val_pipeline = [
     dict(type='PackSegInputs')
 ]
 
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='Resize', scale=(512, 512), keep_ratio=True),
+    dict(type='LoadAnnotations'),  # Optional during real inference; keep if annotations exist
+    dict(type='PackSegInputs')
+]
+
 train_dataloader = dict(
     batch_size=4,
     num_workers=4,
@@ -42,7 +49,18 @@ val_dataloader = dict(
     )
 )
 
-test_dataloader = val_dataloader  # If you want to use val settings for test
+test_dataloader = dict(
+    batch_size=1,
+    num_workers=2,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        data_prefix=dict(img_path='images/val', seg_map_path='annotations/val'),
+        pipeline=test_pipeline,
+    )
+)
 
 val_evaluator = dict(type='IoUMetric', iou_metrics=['mIoU'])
 test_evaluator = val_evaluator
