3dd87225c02a6dfa6348366b2d20a8186d99932c
Agustin Gamio <agamio@urudatasoftware.com>
2025-05-17 20:20:32 -0300

    wip

--- configs/san/san-vit-l14_coco-stuff164k-640x640.py
+++ configs/san/san-vit-l14_coco-stuff164k-640x640.py
@@ -21,6 +21,7 @@ model = dict(
         num_layers=12,
         num_heads=12,
         output_dims=768,
+        vocabulary=['road', 'vegetation', 'sky', 'obstacle', 'others']
     ),
     decode_head=dict(
         type='SideAdapterCLIPHead',
